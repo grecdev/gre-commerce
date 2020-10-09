f52601e1b622ccbb48cf51d0c06bb44f365e4d0b
Grecu Alexandru <grecdev1@gmail.com>
2020-10-09 13:05:28 +0300

    added fonts (#1)

---
+++ src/js/components/home_page/HomeShowcase.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomeShowcase = () => {
+  return (
+    <section id='home-showcase'>
+      <p>Home showcase section</p>
+    </section>
+  );
+};
+
+export default HomeShowcase;
---
+++ src/js/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import HomeShowcase from '@components/home_page/HomeShowcase';
+
+const HomePage = () => {
+  return <>{/* <HomeShowcase /> */}</>;
+};
+
+export default HomePage;
