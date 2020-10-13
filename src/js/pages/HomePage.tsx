95b73269f2cde88b27f84d1d660b9f08b09bf607
Grecu <grecdev1@gmail.com>
2020-10-13 12:57:20 +0300

    added our services section

--- src/js/pages/HomePage.tsx
+++ src/js/pages/HomePage.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import HomeShowcase from '@components/home_page/showcase';
+import OurServices from '@components/home_page/our_services';
 
 const HomePage = () => {
   return (
     <>
       <HomeShowcase />
+      <OurServices />
     </>
   );
 };
